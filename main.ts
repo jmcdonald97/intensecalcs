5ebe82ad9c199e369f7f777a07f8fe71c3b64bf6
cbudischak <105312403+cbudischak@users.noreply.github.com>
2022-05-10 14:00:45 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let StartTime = 0
+let DivideAnswer = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    StartTime = input.runningTimeMicros()
+    for (let index = 0; index < 10000000; index++) {
+        DivideAnswer = 1000000 / 77
+    }
+    basic.showNumber((input.runningTimeMicros() - StartTime) / 1000000)
+})
